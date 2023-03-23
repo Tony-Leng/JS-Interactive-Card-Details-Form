0bffc0deab1090dc8e179b136b60ac4c2c9578f6
Tony Leng <lengtk@gmail.com>
2023-03-23 21:02:38 +0900

    added Confirm button feature in js file

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 console.log('connected');
+
+const confirm = document.querySelector('#submit');
+const thankyou = document.querySelector('#thank-you');
+const userinfo = document.querySelector('#user-info');
+
+confirm.addEventListener('click', () => {
+  thankyou.classList.remove('hide');
+  userinfo.classList.add('hide');
+})
