974a9a68d575ebf3811d60d48211d45e04f89c45
Tony Leng <lengtk@gmail.com>
2023-03-24 19:23:34 +0900

    added event listener with input event for cardholder name display

--- app.js
+++ app.js
@@ -3,8 +3,18 @@ console.log('connected');
 const confirm = document.querySelector('#submit');
 const thankyou = document.querySelector('#thank-you');
 const userinfo = document.querySelector('#user-info');
+const cardholdernameinput = document.querySelector('#cardholder-name-input')
+const cardholdernamedisplay = document.querySelector('#cardholder-name')
 
 confirm.addEventListener('click', () => {
   thankyou.classList.remove('hide');
   userinfo.classList.add('hide');
+})
+
+cardholdernameinput.addEventListener('input', () => {
+  cardholdernamedisplay.innerText = cardholdernameinput.value;
+})
+
+cardholdernameinput.addEventListener('input', () => {
+  cardholdernamedisplay.innerText = cardholdernameinput.value;
 })
